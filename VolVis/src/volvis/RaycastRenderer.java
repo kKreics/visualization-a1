a583a7bb791c1e7b55a2b6ff45a6d6ee0a625bfe
Guillermo Alonso <guialonun@gmail.com>
2017-12-06 17:14:34 +0100

    Implement gradientOpacity function

--- VolVis/src/volvis/RaycastRenderer.java
+++ VolVis/src/volvis/RaycastRenderer.java
@@ -454,6 +454,88 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
     }
 
 
+    void gradientOpacity(double[] viewMatrix) {
+        // clear image
+        int step = interactiveMode ? interactiveStep : 1;
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                image.setRGB(i, j, 0);
+            }
+        }
+
+        // vector uVec and vVec define a plane through the origin,
+        // perpendicular to the view vector viewVec
+        double[] viewVec = new double[3];
+        double[] uVec = new double[3];
+        double[] vVec = new double[3];
+        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
+        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
+        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
+
+        // image is square
+        int imageCenter = image.getWidth() / 2;
+
+        double[] pixelCoord = new double[3];
+        double[] volumeCenter = new double[3];
+        VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
+
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                TFColor voxelColor = new TFColor();
+                TFColor tmpColor = new TFColor();
+
+                pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter)
+                        + volumeCenter[0];
+                pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter)
+                        + volumeCenter[1];
+                pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter)
+                        + volumeCenter[2];
+
+                // Move in the direction of viewVec, starting in the plane
+                // that cuts the figure in two, perpendicular to the viewPlane
+                double[] pixelCoordViewVec = VectorMath.cloneVector(pixelCoord);
+                double[] viewScale = VectorMath.scaleVector(viewVec, step);
+
+                // Advance until the end of the volume (out of bounds)
+                while (getVoxel(pixelCoordViewVec) != -1) {
+                    pixelCoordViewVec = VectorMath.addVectors(pixelCoordViewVec, viewScale);
+                }
+
+                // Reverse the moving vector
+                viewScale = VectorMath.scaleVector(viewScale, -1);
+                // Get inside the volume again
+                pixelCoordViewVec = VectorMath.addVectors(pixelCoordViewVec, viewScale);
+
+                // Iterate the volume until we get out of the volume from the other side
+                while (true) {
+                    short voxelIntensity = getVoxel(pixelCoordViewVec);
+
+                    // We are out of the figure
+                    if (voxelIntensity == -1) {
+                        break;
+                    }
+
+                    tmpColor = tfEditor2D.triangleWidget.color;
+                    tmpColor.a = levoysFormula(pixelCoordViewVec);
+                    voxelColor.r = tmpColor.r * tmpColor.a + (1 - tmpColor.a) * voxelColor.r;
+                    voxelColor.g = tmpColor.g * tmpColor.a + (1 - tmpColor.a) * voxelColor.g;
+                    voxelColor.b = tmpColor.b * tmpColor.a + (1 - tmpColor.a) * voxelColor.b;
+
+                    pixelCoordViewVec = VectorMath.addVectors(pixelCoordViewVec, viewScale);
+                }
+
+                // BufferedImage expects a pixel color packed as ARGB in an int
+                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+                image.setRGB(i, j, pixelColor);
+            }
+        }
+    }
+
+
     private void drawBoundingBox(GL2 gl) {
         gl.glPushAttrib(GL2.GL_CURRENT_BIT);
         gl.glDisable(GL2.GL_LIGHTING);
